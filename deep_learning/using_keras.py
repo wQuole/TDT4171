b1ba10da6d08408ff672727b60fd6b112e5fe868
William Kvaale <wgkvaale@gmail.com>
2019-04-03 23:02:36 +0200

    Completed assignment 5

--- deep_learning/using_keras.py
+++ deep_learning/using_keras.py
@@ -1,63 +1,114 @@
 import pickle
 import tensorflow as tf
+from sys import argv
+
 from os import path
-from tensorflow import keras
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.optimizers import RMSprop
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense
+from keras.utils import plot_model
 
 
 def preprocess_data(filepath):
+    '''
+    Preprocess the data from the pickle file
+    '''
     data = pickle.load(open(filepath, "rb"))
-    X_train, y_train, X_test, y_test, vocab_size, max_length = data["x_train"], data["y_train"], data["x_test"], data[
-        "y_test"], data["vocab_size"], data["max_length"]
+    X_train, y_train, X_test, y_test = data["x_train"], data["y_train"], data["x_test"], data["y_test"]
+    vocab_size, max_length = data["vocab_size"], data["max_length"]
 
-    prepped_X_train = keras.preprocessing.sequence.pad_sequences(sequences=X_train, maxlen=max_length)
-    prepped_X_test = keras.preprocessing.sequence.pad_sequences(sequences=X_test, maxlen=max_length)
-    prepped_y_train = keras.utils.to_categorical(y_train, num_classes=2)
-    prepped_y_test = keras.utils.to_categorical(y_test, num_classes=2)
+    # Dataset to huge for 6 hours of compute
+    partition = round(len(X_train)/200)
 
-    return prepped_X_train, prepped_X_test, prepped_y_train, prepped_y_test, vocab_size, max_length
+    X_train = pad_sequences(sequences=X_train[:partition], maxlen=max_length)
+    X_test = pad_sequences(sequences=X_test[:partition], maxlen=max_length)
+
+    y_train = to_categorical(y_train[:partition], num_classes=2)
+    y_test = to_categorical(y_test[:partition], num_classes=2)
+
+    return X_train, X_test, y_train, y_test, vocab_size, max_length
 
 
-def classify(X_train, X_test, y_train, y_test, vocab_size, max_length):
+def create_model(vocab_size, max_length):
+    '''
+    :param vocab_size: int --> number of different words in vocabulary
+    :param max_length: int --> longest review in dataset
+    :return: keras.engine.sequential.Sequential trained model
+    '''
     # initialize the Sequential model
-    model = keras.Sequential()
+    model = Sequential()
 
     # add layers to the model
-    model.add(keras.layers.Embedding(input_dim=vocab_size, output_dim=128))
-    model.add(keras.layers.LSTM(activation='relu', units=128))
-    model.add(keras.layers.Dense(units=2))
+    model.add(Embedding(input_dim=vocab_size, output_dim=256, input_length=max_length))
+    model.add(LSTM(256))
+    model.add(Dense(2, activation="sigmoid"))
 
     # compile the model
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=keras.optimizers.RMSprop(),
-                  metrics=['accuracy'])
+    # chose RMSProp as the optimizer, since it is usually a good choice for recurrent neural networks.
+    model.compile(optimizer=RMSprop(), loss='binary_crossentropy', metrics=['accuracy'])
 
-    # run on GTX 1080 Ti
-    with tf.device('/GPU:0'):
-        # train the model
-        model.fit(X_train, y_train, batch_size=128, epochs=4, verbose=1)
+    # print model
+    model.summary()
+
+    return model
+
+
+def train(model, X_train, y_train, batch_size=256, epochs=10, device=argv[1].lower()):
+    '''
+    :param model:   untrained keras.engine.sequential.Sequential model
+    :param X_train: numpy.ndarray containing  training data
+    :param y_train: numpy.ndarray containing target (label) data
+    :param batch_size: int --> number of samples per gradient update
+    :param epochs:  int --> number of epochs to train the model
+    :param device:  sys.argv[1] --> user inut 'cpu' or 'gpu'
+    '''
+    # run on GPU --> {kaggle: 'Tesla P100-PCIE-16GB', personal_desktop: 'GTX 1080 Ti'}
+    if device == 'gpu':
+        print(f"\nUsing {device} to train")
+        with tf.device('/GPU:0'):
+            # train the model
+            model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
+
+    # run on CPU --> {personal_macbook: '2,2 GHz i7 8750h', personal_desktop: '5.0 GHz i9 9900k'}
+    else:
+        print(f"Using {device} to train")
+        model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
 
     # save the model
-    model.save("lstm_model.h5")
+    model.save("LSTM_model.h5")
 
-    # predict on test-set
-    pred = model.predict(X=X_test, y=y_test)
-    print('Prediction:\t'.format(pred))
 
+def eval(model, X_test, y_test):
+    '''
+    :param model:   keras.engine.sequential.Sequential --> trained model
+    :param X_test:  numpy.ndarray --> containing test data
+    :param y_test:  numpy.ndarray --> containing target (label) data
+    '''
     # evaluate
-    score = model.evaluate(X_test, y_test, verbose=1)  # Should acquire 90 % accuracy from the LSTM
-    print('Test loss:\t', score[0],
-          '\nTest accuracy:\t', score[1])
-
-
-def get_file_path(file):
-    return path.abspath(file)
+    loss, acc = model.evaluate(X_test, y_test, verbose=1)  # Should acquire at least 90 % accuracy from the LSTM
+    print('Test loss:\t', loss,
+          '\nTest accuracy:\t', acc)
 
 
 def main():
+    # initialize the data
     dataset_sklearn = "data/keras-data.pickle"
-    X_train, X_test, y_train, y_test, vocab_size, max_length = preprocess_data(get_file_path(dataset_sklearn))
-    classify(X_train, X_test, y_train, y_test, vocab_size, max_length)
+    X_train, X_test, y_train, y_test, vocab_size, max_length = preprocess_data(path.abspath((dataset_sklearn)))
 
+    # create model, train it and save it
+    model = create_model(vocab_size, max_length)
+    history = train(model, X_train, y_train)
+
+
+    # evaluate the trained model on test data
+    eval(model, X_test, y_test)
+    plot_model(model=model, show_shapes=1, show_layer_names=1)
+    '''
+    >>> Test loss:        0.1628359776079664
+    >>> Test accuracy:    0.9487619514586909
+    '''
 
 if __name__ == '__main__':
     main()
--- deep_learning/using_sklearn.py
+++ deep_learning/using_sklearn.py
@@ -7,50 +7,62 @@ from sklearn.metrics import accuracy_score
 
 
 
-def _run(filepath):
+def _run(filepath, classifier='dtc'):
+    '''
+    Run method for classifying movie reviews,
+    using Decision Tree Classifier and Bernoulli Naive Bayes
+    '''
+    # prepare data
     data = pickle.load(open(filepath, "rb"))
     x_train, y_train, x_test, y_test = data["x_train"], data["y_train"], data["x_test"], data["y_test"]
+
+    # init vectorizer
     vector = HashingVectorizer(stop_words="english", binary=True)
-    transformed_x_train = vector.fit_transform(x_train)
-    transformed_x_test = vector.fit_transform(x_test)
+
+    # transform the training and test data
+    transformed_X_train = vector.fit_transform(x_train)
+    transformed_X_test = vector.fit_transform(x_test)
+
 
     '''DecisionTreeClassifier'''
-    dtc_classifier = DecisionTreeClassifier(criterion="gini")
+    dtc_classifier = DecisionTreeClassifier(criterion="entropy")
 
     # fit the transformed data
-    dtc_classifier.fit(X=transformed_x_train,y=y_train)
+    dtc_classifier.fit(transformed_X_train, y_train)
 
     # make predictions for the test set
-    dtc_pred = dtc_classifier.predict(X=transformed_x_test)
+    dtc_pred = dtc_classifier.predict(transformed_X_test)
 
 
     '''Naive Bayes w/BernoulliNB'''
-    nb_classifier = BernoulliNB()
+    bnb_classifier = BernoulliNB()
 
     # fit the transformed data
-    nb_classifier.fit(X=transformed_x_train,y=y_train)
+    bnb_classifier.fit(transformed_X_train, y_train)
 
     # make predictions for the test set
-    nb_pred = nb_classifier.predict(X=transformed_x_test)
+    bnb_pred = bnb_classifier.predict(transformed_X_test)
 
 
     '''Accuracy'''
-    print_accuracy(nb_pred, y_test)
+    if classifier == 'dtc':
+        print_accuracy(dtc_pred, y_test)
+    elif classifier == 'bnb':
+        print_accuracy(bnb_pred, y_test)
+    else:
+        print("Choose between the two classifiers 'dtc' and 'bnb'")
 
-    # DecisionTreeClassifier --> The accuracy for this classifier:	0.8645118288796274
-    # BernoulliNB -->  The accuracy for this classifier:	0.8380500735474381
+    # DecisionTreeClassifier    --> The accuracy for this classifier:	0.8645118288796274
+    # BernoulliNB               -->  The accuracy for this classifier:	0.8380500735474381
 
 
 def print_accuracy(y_pred, y_true):
-    print("The accuracy for this classifier:\t{}".format(accuracy_score(y_pred, y_true)))
-
-def get_file_path(file):
-    return path.abspath(file)
+    print(f"The accuracy for this classifier:\t{accuracy_score(y_pred, y_true)}")
 
 
 def main():
     dataset_sklearn = "data/sklearn-data.pickle"
-    _run(get_file_path(dataset_sklearn))
+    _run(path.abspath(dataset_sklearn), classifier='bnb')
 
 
 if __name__ == '__main__':
